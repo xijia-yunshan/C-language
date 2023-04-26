e416cb748b91632e2e784e713c4fcb02436fb846
xijia-yunshan <3163686175@qq.com>
2023-04-26 22:44:58 +0800

    打印小飞机

---
+++ test-2023-4-26/test-2023-4-26/打印小飞机.c
@@ -0,0 +1,13 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main()
+{
+	printf("     **\n");
+	printf("     **\n");
+	printf("************\n");
+	printf("************\n");
+	printf("    *  *\n");
+	printf("    *  *\n");
+	return 0;
+}
