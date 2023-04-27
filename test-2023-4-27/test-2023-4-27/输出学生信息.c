b4902c27cfb55992763294d7870a13cd3a0b41e5
xijia-yunshan <3163686175@qq.com>
2023-04-27 19:08:55 +0800

    输出学生信息

---
+++ test-2023-4-27/test-2023-4-27/输出学生信息.c
@@ -0,0 +1,11 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main()
+{
+	printf("Name    Age    Gender\n");
+	printf("---------------------\n");
+	printf("Jack    18     man\n");
+	return 0;
+}
+//输出学生信息
