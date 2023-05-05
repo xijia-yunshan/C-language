26e12550b6960be13a83361796616b3ae3a91c69
xijia-yunshan <3163686175@qq.com>
2023-05-05 20:17:23 +0800

    转换ASCII码值为对应字符

---
+++ test-2023-5-5/test-2023-5-5/转换ASCII码值为对应字符.c
@@ -0,0 +1,15 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main()
+{
+	int arr[] = { 73, 32, 99, 97, 110, 32, 100, 111, 32, 105, 116, 33 };
+	int i = 0;
+	int sz = sizeof(arr) / sizeof(arr[0]);
+	while (i < sz)
+	{
+		printf("%c", arr[i]);
+		i++;
+	}
+	return 0;
+}
