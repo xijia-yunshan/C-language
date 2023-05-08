c610d610f908edd6c9d4b2d1cbc2a1ece9e7c4ad
xijia-yunshan <130556554+xijia-yunshan@users.noreply.github.com>
2023-05-08 13:55:40 +0800

    数组查找元素

---
+++ test-2023-5-8(3)/test-2023-5-8(3)/数组查找元素.c
@@ -0,0 +1,23 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main()
+{
+	int arr[10] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 }; 
+	int k = 11;
+	int i = 0;
+	int sz = sizeof(arr) / sizeof(arr[0]);
+	for (i = 0; i <= sz; i++)
+	{
+		if (arr[i] == k)
+		{
+            printf("找到了，下标是：%d\n", i);
+			break;
+		}	
+	}
+	if (i > sz)
+	{
+		printf("找不到\n");
+	}
+	return 0;
+}
