da2d86d8b485a2021f51189efe6adf5e5d0511aa
xijia-yunshan <130556554+xijia-yunshan@users.noreply.github.com>
2023-05-12 16:06:15 +0800

    打印100-200之间的素数

---
+++ test-2023-5-12(3)/test-2023-5-12(3)/打印100-200之间的素数.c
@@ -0,0 +1,26 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main()
+{
+	int i = 0;
+	int counter = 0;
+	for (i = 100; i <= 200; i++)
+	{
+		int j = 0;
+		for (j = 2; j < i; j++)
+		{
+			if (i%j == 0)
+			{
+				break;
+			}
+		}
+		if (j == i)
+		{
+			printf("%d ", i);
+			counter++;
+		}	
+	}
+	printf("\nËØÊý¸öÊý£º%d\n", counter);
+	return 0;
+}
