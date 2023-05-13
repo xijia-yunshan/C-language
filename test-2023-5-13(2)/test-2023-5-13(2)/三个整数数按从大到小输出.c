9805fd1ab02f08c2fbc697b901af26312058fba0
xijia-yunshan <130556554+xijia-yunshan@users.noreply.github.com>
2023-05-13 13:56:43 +0800

    三个整数数按从大到小输出

---
+++ test-2023-5-13(2)/test-2023-5-13(2)/三个整数数按从大到小输出.c
@@ -0,0 +1,30 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main()
+{
+	int a = 0;
+	int b = 0;
+	int c = 0;
+	scanf("%d %d %d", &a, &b, &c);
+	if (a < b)
+	{
+		int d = a;
+		a = b;
+		b = d;
+	}
+	if (a < c)
+	{
+		int d = a;
+		a = c;
+		c = d;
+	}
+	if (b < c)
+	{
+		int d = b;
+		b = c;
+		c = d;
+	}
+	printf("从大到小排列：%d %d %d\n", a, b, c);
+	return 0;
+}
