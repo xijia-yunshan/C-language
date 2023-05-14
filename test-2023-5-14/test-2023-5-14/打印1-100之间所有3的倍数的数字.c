d95d49d8ba1bb225ad916fc1520004f0da0ccce7
xijia-yunshan <130556554+xijia-yunshan@users.noreply.github.com>
2023-05-14 16:07:07 +0800

    打印1-200之间所有3的倍数的数字

---
+++ test-2023-5-14/test-2023-5-14/打印1-100之间所有3的倍数的数字.c
@@ -0,0 +1,18 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main()
+{
+	int i = 0;
+	int counter = 0;
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0)
+		{
+			printf("%d ", i);
+			counter++;
+		}
+	}
+	printf("\ncounter=%d\n", counter);
+	return 0;
+}
