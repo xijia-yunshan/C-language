4598ba9f2033360c909dcf86dc5bce1d924d3e5e
xijia-yunshan <130556554+xijia-yunshan@users.noreply.github.com>
2023-07-02 23:18:29 +0800

    简单三子棋游戏
    代码运行起来不够精确

---
+++ test-2023-6-24/test-2023-6-24/game.c
@@ -0,0 +1,129 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include "game.h"
+
+void InitBoard(char board[ROW][COL], int row, int col)
+{
+	int i = 0;
+	int j = 0;
+	for (i = 0; i < row; i++)
+	{
+		for (j = 0; j < col; j++)
+		{
+			board[i][j] = ' ';
+		}
+	}
+}
+
+void DisplayBoard(char board[ROW][COL], int row, int col)
+{
+	int i = 0;
+	int j = 0;
+	for (i = 0; i < row; i++)
+	{
+		for (j = 0; j < col; j++)
+		{
+			printf(" %c ", board[i][j]);
+			if (j<col - 1)
+				printf("|");
+		}
+		printf("\n");
+		if (i < row - 1)
+		{
+			for (j = 0; j < col; j++)
+			{
+				printf("---");
+				if (j<col - 1)
+					printf("|");
+			}
+		}
+		printf("\n");
+	}
+}
+
+void PlayerMove(char board[ROW][COL], int row, int col)
+{
+	int x = 0;
+	int y = 0;
+	printf("玩家下棋:>\n");
+	while (1)
+	{
+		printf("请输入你要下棋的坐标:>");
+		scanf("%d %d", &x, &y);
+		if ((x >= 1 && x <= row) && (y >= 1 && y <= col))
+		{
+			if (board[x - 1][y - 1] == ' ')
+			{
+				board[x - 1][y - 1] = '*';
+				break;
+			}
+			else
+			{
+				printf("位置已被占用，请重新输入\n");
+			}
+		}
+		else
+		{
+			printf("坐标无效，请重新输入\n");
+		}
+	}
+}
+
+void ComputerMove(char board[ROW][COL], int row, int col)
+{
+	int x = 0;
+	int y = 0;
+	printf("电脑下棋:>\n");
+	while (1)
+	{
+		x = rand() % row;//0~2
+		y = rand() % col;//0~2
+		if (board[x][y] == ' ')
+		{
+			board[x][y] = '#';
+			break;
+		}
+	}
+}
+
+int IsFull(char board[ROW][COL], int row, int col)
+{
+	int i = 0;
+	int j = 0;
+	for (i = 0; i < row; i++)
+	{
+		for (j = 0; j < col; j++)
+		{
+			if (board[i][j] == ' ')
+				return 0;
+		}
+	}
+	return 1;
+}
+
+//玩家赢 -- '*'
+//电脑赢 -- '#'
+//平局   -- 'Q'
+//继续   -- 'C'
+char IsWin(char board[ROW][COL], int row, int col)
+{
+	int i = 0;
+	for (i = 0; i < row; i++)
+	{
+		if (board[i][0] == board[i][1] && board[i][1] == board[i][2] && board[i][0] != ' ')
+			return board[i][0];
+	}
+	for (i = 0; i < col; i++)
+	{
+		if (board[0][i] == board[1][i] && board[1][i] == board[2][i] && board[0][i] != ' ')
+			return board[0][i];
+	}
+	if (board[0][0] == board[1][1] && board[1][1] == board[2][2] && board[0][0] != ' ')
+		return board[0][0];
+	if (board[0][2] == board[1][1] && board[1][1] == board[2][0] && board[0][2] != ' ')
+		return board[0][0];
+	if (1 == IsFull(board, row, col))
+	{
+		return 'Q';
+	}
+	return 'C';
+}
---
+++ test-2023-6-24/test-2023-6-24/三子棋.c
@@ -0,0 +1,65 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include "game.h"
+
+void menu()
+{
+	printf("**********************\n");
+	printf("********1.play********\n");
+	printf("********0.exit********\n");
+	printf("**********************\n");
+}
+
+void game()
+{
+	srand((unsigned int)time(NULL));
+	char board[ROW][COL] = { 0 };
+	InitBoard(board, ROW, COL);
+	DisplayBoard(board, ROW, COL);
+	char ret = 0;
+	while (1)
+	{
+		PlayerMove(board, ROW, COL);
+		DisplayBoard(board, ROW, COL);
+		ret = IsWin(board, ROW, COL);
+		if (ret != 'C')
+		{
+			break;
+		}
+		ComputerMove(board, ROW, COL);
+		DisplayBoard(board, ROW, COL);
+		if (ret != 'C')
+		{
+			break;
+		}
+	}
+	if ('*' == ret)
+		printf("玩家赢\n");
+	else if ('#' == ret)
+		printf("电脑赢\n");
+	else
+		printf("平局\n");
+}
+
+int main()
+{
+	int input = 0;
+	do
+	{
+		menu();
+		printf("请选择:>");
+		scanf("%d", &input);
+		switch (input)
+		{
+		case 1:
+			game();
+			break;
+		case 0:
+			printf("退出游戏\n");
+			break;
+		default:
+			printf("选择错误,重新选择\n");
+			break;
+		}
+	} while (input);
+	return 0;
+}
