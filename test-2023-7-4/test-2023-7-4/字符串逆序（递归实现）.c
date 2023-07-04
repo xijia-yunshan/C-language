0581cc06ec08d664b4ff8b29a5cb3e03bfe29da5
xijia-yunshan <130556554+xijia-yunshan@users.noreply.github.com>
2023-07-04 23:43:06 +0800

    字符串逆序（递归实现）

---
+++ test-2023-7-4/test-2023-7-4/字符串逆序（递归实现）.c
@@ -0,0 +1,25 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <string.h>
+#include <stdio.h>
+
+void reverse_string(char* arr)
+{
+	int len = strlen(arr);
+	char tmp = *arr;
+	*arr = *(arr + len - 1);
+	*(arr + len - 1) = '\0';
+	if (strlen(arr + 1) >= 2)
+	{
+		reverse_string(arr + 1);
+	}
+		*(arr + len - 1) = tmp;
+}
+
+int main()
+{
+	char arr[] = "abcdef";
+	printf("交换前：%s\n", arr);
+	reverse_string(&arr);
+	printf("交换后：%s\n", arr);
+	return 0;
+}
