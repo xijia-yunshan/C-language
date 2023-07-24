7909d699e77933fa3526902340799aa1e89116a9
xijia-yunshan <130556554+xijia-yunshan@users.noreply.github.com>
2023-07-24 23:12:19 +0800

    100-200之间的素数(平方法)

---
+++ test-2023-7-24/test-2023-7-24/100-200之间的素数(开平方).c
@@ -0,0 +1,29 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <math.h>
+#include <stdio.h>
+
+int main()
+{
+	int i = 0;
+	int count = 0;
+	for (i = 101; i < 200; i += 2)
+	{
+		int flag = 1;
+		int j = 0;
+		for (j = 2; j <= sqrt(i); j++)
+		{
+			if (i%j == 0)
+			{
+				flag = 0;
+				break;
+			}
+		}
+		if (flag == 1)
+		{
+			printf("%d ", i);
+			count++;
+		}
+	}
+	printf("\ncount=%d\n", count);
+	return 0;
+}
