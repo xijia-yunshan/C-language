d5747abc2765614825120c54af451b5eb594b078
xijia-yunshan <130556554+xijia-yunshan@users.noreply.github.com>
2023-07-27 23:46:07 +0800

    求最大公约数（穷举法）

---
+++ test-2023-7-27(2)/test-2023-7-27(2)/求最大公约数(穷举法).c
@@ -0,0 +1,23 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main()
+{
+	int a = 0;
+	int b = 0;
+	int i = 0;
+	scanf("%d%d", &a, &b);
+	if (a > b)
+		i = b;
+	else
+		i = a;
+	for (int j = i; j > 0; j--)
+	{
+		if (a % j == 0 && b % j == 0)
+		{
+			printf("最大公约数为：%d\n", j);
+			break;
+		}
+	}
+	return 0;
+}
