4ec85bf2ddd59b8340be37aa37260d9d04e4ed4e
xijia-yunshan <130556554+xijia-yunshan@users.noreply.github.com>
2023-08-07 22:45:22 +0800

    单身狗（异或法）
    注意：这种方法只适用于数组中只有一个单独数字的情况！

---
+++ test-2023-8-7/test-2023-8-7/单身狗（异或法）.c
@@ -0,0 +1,21 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main()
+{
+	int i = 0;
+	int n = 0;
+	scanf("%d", &n);
+	int arr[1000] = { 0 };
+	for (i = 0; i < n; i++)
+	{
+		scanf("%d", &arr[i]);
+	}
+	int ret = 0;
+	for (i = 0; i < n; i++)
+	{
+		ret ^= arr[i];
+	}
+	printf("%d\n", ret);
+	return 0;
+}
