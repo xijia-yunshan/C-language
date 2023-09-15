37fa43c6c1f4a2c8fad209b49f41faf868502856
xijia-yunshan <130556554+xijia-yunshan@users.noreply.github.com>
2023-09-16 00:13:01 +0800

    strcpy模拟实现（版本2）

---
+++ test-2023-9-16(2)/test-2023-9-16(2)/strcpy模拟实现（版本2）.c
@@ -0,0 +1,24 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <string.h>
+
+void my_strcpy(char* dest, char* src)
+{
+	//拷贝的是'\0'之前的字符
+	while (*src != '\0')
+	{
+		*dest++ = *src++;
+	}
+	//拷贝'\0'字符
+	*dest = *src;
+}
+
+int main()
+{
+	char arr1[] = "abcdef";
+	char arr2[20] = { 0 };
+
+	my_strcpy(arr2, arr1);
+	printf("%s\n", arr2);
+	return 0;
+}
