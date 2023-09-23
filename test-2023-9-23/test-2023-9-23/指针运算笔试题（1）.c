213c39e31c903cb2cf9785aa4354cf129f194519
xijia-yunshan <130556554+xijia-yunshan@users.noreply.github.com>
2023-09-23 23:56:34 +0800

    指针运算笔试题（1）

---
+++ test-2023-9-23/test-2023-9-23/指针运算笔试题（1）.c
@@ -0,0 +1,10 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main()
+{
+	int a[5] = { 1, 2, 3, 4, 5 };
+	int *ptr = (int *)(&a + 1);
+	printf("%d,%d", *(a + 1), *(ptr - 1));
+	return 0;
+}
