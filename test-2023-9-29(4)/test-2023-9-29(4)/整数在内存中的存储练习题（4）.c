0adb9dd29b826ccb1add3a38860189d7276bc0e7
xijia-yunshan <130556554+xijia-yunshan@users.noreply.github.com>
2023-09-30 00:12:39 +0800

    整数在内存中的存储练习题（4）

---
+++ test-2023-9-29(4)/test-2023-9-29(4)/整数在内存中的存储练习题（4）.c
@@ -0,0 +1,21 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+unsigned char i = 0;//0~255
+int main()
+{
+	//À¿—≠ª∑
+	for (i = 0; i <= 255; i++)
+	{
+		printf("hello world\n");
+	}
+	return 0;
+}//int main()//{
+//	unsigned int i;
+//
+//	for (i = 9; i >= 0; i--)
+//	{
+//		printf("%u\n", i);
+//	}
+//
+//	return 0;//}
