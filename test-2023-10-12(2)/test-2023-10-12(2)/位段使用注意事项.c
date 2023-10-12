c3121344e463419f0fd5be6784968976ad96b4bc
xijia-yunshan <130556554+xijia-yunshan@users.noreply.github.com>
2023-10-12 23:58:53 +0800

    位段使⽤的注意事项
    位段中可能会出现几个成员共有同⼀个字节情况，这样有些成员的起始位置并不是某个字节的起始位置，那么这些位置处是没有地址的。内存中每个字节分配⼀个地址，⼀个字节内部的bit位是没有地址的。
    所以不能对位段的成员使用&操作符，这样就不能使用scanf直接给位段的成员输入值，只能是先输入放在⼀个变量中，然后赋值给位段的成员。

---
+++ test-2023-10-12(2)/test-2023-10-12(2)/位段使用注意事项.c
@@ -0,0 +1,20 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+
+struct A
+{
+	int _a : 2;
+	int _b : 5;
+	int _c : 10;
+	int _d : 30;
+};
+int main()
+{
+	struct A sa = { 0 };
+	scanf("%d", &sa._b);//这是错误的
+
+	//正确的⽰范
+	int b = 0;
+	scanf("%d", &b);
+	sa._b = b;
+	return 0;
+}
