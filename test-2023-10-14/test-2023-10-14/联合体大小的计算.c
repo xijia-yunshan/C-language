9dd7a99a617533b5221696fa741b051815aba82d
xijia-yunshan <130556554+xijia-yunshan@users.noreply.github.com>
2023-10-14 23:58:28 +0800

    联合体大小的计算
    联合的大小至少是最⼤成员的大小。
    当最⼤成员大小不是最大对齐数的整数倍的时候，就要对齐到最大对齐数的整数倍。

---
+++ test-2023-10-14/test-2023-10-14/联合体大小的计算.c
@@ -0,0 +1,19 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+union Un1
+{
+	char c[5];//5 —> 对齐数按成员来算 1 8 1
+	int i;//4                          4 8 4
+};
+union Un2
+{
+	short c[7];//14 —> 对齐数按成员来计算 2 8 2
+	int i;//4                              4 8 4
+};
+int main()
+{
+	//下⾯输出的结果是什么？
+	printf("%d\n", sizeof(union Un1));
+	printf("%d\n", sizeof(union Un2));
+	return 0;
+}//联合体的大小，是其中最大成员的大小（这句话是错误的）
