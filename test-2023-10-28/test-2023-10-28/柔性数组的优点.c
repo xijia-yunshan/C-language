d3adac560674f196eee8952895f723addd7c6330
xijia-yunshan <130556554+xijia-yunshan@users.noreply.github.com>
2023-10-29 00:22:14 +0800

    柔性数组的优点
    第⼀个好处是：⽅便内存释放
    如果我们的代码是在⼀个给别⼈⽤的函数中，你在⾥⾯做了⼆次内存分配，并把整个结构体返回给用户。用户调⽤free可以释放结构体，但是⽤⼾并不知道这个结构体内的成员也需要free，所以你不能指望用户来发现这个事。所以，如果我们把结构体的内存以及其成员要的内存⼀次性分配好了，并返回给用户⼀个结构体指针，用户做⼀次free就可以把所有的内存也给释放掉。
    
    第⼆个好处是：这样有利于访问速度。
    连续的内存有益于提⾼访问速度，也有益于减少内存碎⽚。（其实，我个⼈觉得也没多⾼了，反正你跑不了要⽤做偏移量的加法来寻址）

---
+++ test-2023-10-28/test-2023-10-28/柔性数组的优点.c
@@ -0,0 +1,61 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+
+//不使用柔性数组的情况下，st_type使用下面这种结构，也能达到同样的效果
+
+struct st_type
+{
+	int i;
+	int* a;
+};
+
+int main()
+{
+	struct st_type* p = (struct st_type*)malloc(sizeof(struct st_type));
+	if (p == NULL)
+	{
+		perror("malloc");
+		return;
+	}
+
+	p->i = 100;
+	p->a = (int*)malloc(10 * sizeof(int));
+	if (p->a == NULL)
+	{
+		perror("malloc");
+		return;
+	}
+	for (int i = 0; i < 10; i++)
+	{
+		p->a[i] = i;
+	}
+	//希望a指向的空间为60个字节
+	struct st_type* ptr = (struct st_type*)realloc(p->a, 15 * sizeof(int));
+	if (ptr == NULL)
+	{
+		perror("realloc");
+		return;
+	}
+	else
+	{
+		p = ptr;
+		ptr = NULL;
+	}
+	//使用
+	//...
+	//释放
+	free(p->a);
+	free(p);
+	p = NULL;
+
+	return 0;
+}
+
+//柔性数组的优点：
+
+//第⼀个好处是：⽅便内存释放
+//未使用柔性数组需要两次或多次释放内存
+
+//第⼆个好处是：这样有利于访问速度
+//柔性数组是连续存放，而非柔性数组不是连续的，他们之间存在空隙，即内存碎片
