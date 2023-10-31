beb7114f556624d763d5ebb67f8bdb31f0da8ae4
xijia-yunshan <130556554+xijia-yunshan@users.noreply.github.com>
2023-10-31 23:56:26 +0800

    malloc函数动态开辟二维数组（1）

---
+++ test-2023-10-31/test-2023-10-31/malloc函数动态开辟二维数组（1）.c
@@ -0,0 +1,54 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+	int** p = (int**)malloc(3 * sizeof(int*));//开辟行
+
+	int i = 0;
+	for (i = 0; i < 3; i++)
+	{
+		*(p + i) = (int*)malloc(5 * sizeof(int));//开辟列
+	}
+	
+	for (i = 0; i < 3; i++)
+	{
+		int j = 0;
+		for (j = 0; j < 5; j++)
+		{
+			scanf("%d", &p[i][j]);
+		}
+	}
+
+	for (i = 0; i < 3; i++)
+	{
+		int j = 0;
+		for (j = 0; j < 5; j++)
+		{
+			printf("%2d ", p[i][j]);
+		}
+		printf("\n");
+	}
+
+	printf("\n");
+
+	for (i = 0; i < 3; i++)
+	{
+		int j = 0;
+		for (j = 0; j < 5; j++)
+		{
+			printf("%p ", &p[i][j]);
+		}
+		printf("\n");
+	}
+
+
+	//释放
+	for (i = 0; i < 3; i++)
+	{
+		free(*(p + i));
+	}
+		
+	return 0;
+}
