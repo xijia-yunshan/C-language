08a44e5b6ad40540942e160c2064f8621e21080b
xijia-yunshan <130556554+xijia-yunshan@users.noreply.github.com>
2023-11-17 22:51:50 +0800

    文件缓冲区
    ANSIC标准采用“缓冲⽂件系统”处理的数据⽂件的，所谓缓冲文件系统是指系统自动地在内存中为程序中每⼀个正在使⽤的⽂件开辟⼀块“文件缓冲区”。从内存向磁盘输出数据会先送到内存中的缓冲区，装满缓冲区后才⼀起送到磁盘上。如果从磁盘向计算机读⼊数据，则从磁盘⽂件中读取数据输入到内存缓冲区（充满缓冲区），然后再从缓冲区逐个地将数据送到程序数据区（程序变量等）。缓冲区的大小根据C编译系统决定的。

---
+++ test-2023-11-17/文件缓冲区.c
@@ -0,0 +1,30 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <windows.h>
+
+//我们写了一个程序，程序中有各种变量，例如：结构体、数组，我们想把他们写到文件中去，而文件存放在硬盘上，把数据写到文件中，不是直接把文件写道硬盘上
+//而是先把数据放到缓冲区里面，缓冲区放满，或者我们手动刷新缓冲区，才会把缓冲区里面的数据写到硬盘中去
+//我们读文件也是一样的，先放到缓冲区，再传递到程序中去
+
+
+int main()
+{
+	FILE* pf = fopen("test.txt", "w");
+	fputs("abcdef", pf);//先将代码放在输出缓冲区
+	printf("睡眠10秒已经写数据了，打开test.txt文件，发现文件没有内容\n");
+	
+	Sleep(10000);
+	
+	printf("刷新缓冲区\n");
+	
+	fflush(pf);//刷新缓冲区时，才将输出缓冲区的数据写到⽂件
+	
+	printf("再睡眠10秒此时，再次打开test.txt文件，文件有内容了\n");
+	
+	Sleep(10000);
+	
+	fclose(pf);//fclose在关闭⽂件的时候，也会刷新缓冲区
+	
+	pf = NULL;
+	return 0;
+}
