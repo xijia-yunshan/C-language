71990ac7bceed9ec8bea3c0888496c02c99aa271
xijia-yunshan <130556554+xijia-yunshan@users.noreply.github.com>
2025-08-13 00:02:35 +0800

    利用静态库求解析get_days_of_month函数求某年某月天数

---
+++ get_days_of_month.h
@@ -0,0 +1,2 @@
+#pragma once
+int get_days_of_month(int y, int m);
---
+++ 利用静态库求解析函数求某年某月天数.c
@@ -0,0 +1,16 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h> 
+
+#include "get_days_of_month.h"
+
+#pragma comment(lib,"get_days_of_month.lib")
+
+int main()
+{
+	int year = 0;
+	int month = 0;
+	scanf("%d %d", &year, &month);
+	int days = get_days_of_month(year, month);
+	printf("%d\n", days);
+	return 0;
+}
